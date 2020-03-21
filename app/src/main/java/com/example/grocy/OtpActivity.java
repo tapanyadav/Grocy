6adf84b7289289071c9851c12da35217048c6464
Tapan Yadav <yadav.tapanpr@gmail.com>
2020-03-21 13:06:35 +0530

    Update OtpActivity.java
    minor updates in variables and string code..

--- app/src/main/java/com/example/grocy/OtpActivity.java
+++ app/src/main/java/com/example/grocy/OtpActivity.java
@@ -30,12 +30,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class OtpActivity extends AppCompatActivity {
 
     private static final String TAG ="OTP ACTIVITY" ;
-    EditText et1;
-    EditText et2;
-    EditText et3;
-    EditText et4;
-    EditText et5;
-    EditText et6;
+    EditText et1,et2,et3,et4,et5,et6;
 
     Button btnOtpLogin;
     private FirebaseAuth mAuth;
@@ -183,7 +178,7 @@ public class OtpActivity extends AppCompatActivity {
                             // ...
                         } else {
                             // Sign in failed, display a message and update the UI
-                            System.out.println("jcssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+                            System.out.println("Sign in  failed");
                             Log.w("TAG", "signInWithCredential:failure", task.getException());
                             Toast.makeText(OtpActivity.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
